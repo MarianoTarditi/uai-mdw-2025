9da19069d3d3385b0d3e4a8e3f3e0312dd5d3718
Mariano Tarditi <mariano_tarditi@hotmail.com>
2025-09-02 12:23:19 -0300

    index.ts modified

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    console.error("MongoDB connection URI is missing!");
+    process.exit(1); // detener el servidor si no hay URI
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI || "");
+    await mongoose.connect(mongoUri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./database";
+import connectDB from "./config/database";
 import router from "./routes";
 
 dotenv.config();
